669bf38e6c547f18f5c7c40f1f99d780c012c9d7
goldwolf <goldwolf.dev@gmail.com>
2023-11-09 03:33:32 -0400

    login register start

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Home } from './pages';
 import { View } from './pages'
 import { CheckedList } from './pages'
 import { Assignment } from './pages'
-
+import Login  from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+// import Navbar from "../src/pages/layout/Navbar";
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,12 +24,25 @@ const router = createBrowserRouter([
     path: '/assignment',
     element: <Assignment />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  // {
+  //   path: '/curator-video-list',
+  //   element: <CuratorPanel />,
+  // },
 ]);
 
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router} />
+      {/* <Navbar /> */}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as View } from './View';
 export { default as CheckedList } from './CheckedList';
 export { default as Assignment } from './Assignment';
+// export { default as Login } from './Auth';
