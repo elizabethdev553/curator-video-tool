0e33a48070e5852a37bf247c893bc9ecbec0723e
goldwolf <goldwolf.dev@gmail.com>
2023-11-12 22:56:28 -0400

    server.js modified to build

--- mongo_backend/server.js
+++ mongo_backend/server.js
@@ -20,13 +20,10 @@ app.use('/api/curator', require('./routes/api/curator'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+ 
 }
 
+app.use(express.static(path.join(__dirname, 'dist')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
